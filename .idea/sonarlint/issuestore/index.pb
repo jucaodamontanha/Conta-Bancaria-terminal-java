378d24b85bcb5de060dc41f4861edcc7d7f041a8
jucaodamontanha <jucaodamontanha@gmail.com>
2024-07-18 00:17:38 -0300

    criado arquivo java

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+?
+ContaBanco.java,c/9/c930bb2ee441c59f0afd6e84f45ac4c65b65aae7
+C
+scr/ContaBanco.java,a/2/a2affff797d10ed6919f6309f87694723898fc9b
---
+++ scr/ContaBanco.java
@@ -0,0 +1,7 @@
+package scr;
+
+public class ContaBanco {
+    public static void main(String[] args) {
+        System.out.println(" Ola, mundo");
+    }
+}
